b82d791f67a0404ead6d8e76355a4feaa7ed1175
korobasssss <an.korobeynikova@gmail.com>
2024-11-14 15:08:20 +0300

    commit declarations

--- src/lib/Select/Select.d.ts
+++ src/lib/Select/Select.d.ts
@@ -12,6 +12,6 @@ export interface ISelect<T extends string | number, K extends string> {
     disabled?: boolean;
     allowClear?: boolean;
     showSearch?: boolean;
-    error: string;
+    error?: string;
 }
 export declare const MySelect: <T extends string | number, K extends string>({ value, options, onChange, placeholder, multiply, disabled, allowClear, showSearch, error }: ISelect<T, K>) => JSX.Element;
