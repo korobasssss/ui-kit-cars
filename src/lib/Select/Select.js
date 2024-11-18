e41f298b6729c04bfd3050397d6c6fe7e9da51d3
korobasssss <an.korobeynikova@gmail.com>
2024-11-18 17:14:07 +0300

    commit declarations

--- src/lib/Select/Select.js
+++ src/lib/Select/Select.js
@@ -4,6 +4,6 @@ import './styles.scss';
 import { Message } from "../Message";
 export const MySelect = ({ value, options, onChange, placeholder, multiply, disabled, allowClear = true, showSearch = false, error }) => {
     return (_jsxs("div", { children: [_jsx(Select, { value: value, placeholder: placeholder, mode: multiply, onChange: onChange, disabled: disabled, allowClear: allowClear, showSearch: showSearch, className: 'SSelect', notFoundContent: 'Нет подходящих опций', children: options?.map((option) => {
-                    return (_jsx(Select.Option, { value: option.value, children: option.label }));
+                    return (_jsx(Select.Option, { value: option.value, children: option.label }, option.value));
                 }) }), error && (_jsx(Message, { type: 'error', message: error }))] }));
 };
