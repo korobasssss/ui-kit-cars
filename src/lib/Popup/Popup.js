475c0103470183d8a988b20d9e5b947ff8802bf1
korobasssss <an.korobeynikova@gmail.com>
2024-11-20 09:30:28 +0300

    commit buildings files

--- src/lib/Popup/Popup.js
+++ src/lib/Popup/Popup.js
@@ -2,5 +2,5 @@ import { jsx as _jsx } from "react/jsx-runtime";
 import { Modal } from "antd";
 import './styles.scss';
 export const Popup = ({ title, isModalOpen, children, handleClose }) => {
-    return (_jsx(Modal, { title: title, open: isModalOpen, onCancel: () => handleClose(false), footer: null, zIndex: 10, width: 679, children: isModalOpen && (children) }));
+    return (_jsx(Modal, { title: title, open: isModalOpen, onCancel: () => handleClose(false), footer: null, zIndex: 10, width: 679, forceRender: true, children: children }));
 };
